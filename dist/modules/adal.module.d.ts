fdfcfa6de17497dc26b7030af6b9771da888774c
Murali Krishnan Elangovan <mkelan@microsoft.com>
2017-03-16 22:53:33 -0700

    -fix

--- dist/modules/adal.module.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * ng2-adal - Use Azure AD Library - ADAL in Angular 2
- * @version v0.2.8
- * @link https://github.com/sureshchahal/angular2-adal#readme
- * @license MIT
- */
-export declare class AdalModule {
-}
